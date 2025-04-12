eb9981568d7b9ce6da071ff0ec09e60ea5187dff
Daochun Zhao <daochun.zhao@lego.com>
2025-04-12 21:06:58 +0800

    implement it in a generic way

--- internal/controller/namespaceclass_controller.go
+++ internal/controller/namespaceclass_controller.go
@@ -17,35 +17,80 @@ limitations under the License.
 package controller
 
 import (
-    "context"
-    "fmt"
+	"context"
+	"fmt"
 
-    "k8s.io/api/core/v1"
-    networkingv1 "k8s.io/api/networking/v1"
-    "k8s.io/apimachinery/pkg/api/errors"
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-    "k8s.io/apimachinery/pkg/labels"
-    "k8s.io/apimachinery/pkg/runtime"
-    ctrl "sigs.k8s.io/controller-runtime"
-    "sigs.k8s.io/controller-runtime/pkg/builder"
-    "sigs.k8s.io/controller-runtime/pkg/client"
-    "sigs.k8s.io/controller-runtime/pkg/event"
-    "sigs.k8s.io/controller-runtime/pkg/handler"
-    "sigs.k8s.io/controller-runtime/pkg/log"
-    "sigs.k8s.io/controller-runtime/pkg/predicate"
-    "sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-    corev1 "akuity.io/homework/api/v1"
+	corev1 "akuity.io/homework/api/v1"
 )
 
 const (
-    NamespaceLabelName = "namespaceclass.akuity.io/name"
+	NamespaceLabelName = "namespaceclass.akuity.io/name"
+
+	ResourceNetworkPolicy  = "networkPolicy"
+	ResourceServiceAccount = "serviceAccount"
 )
 
 // NamespaceClassReconciler reconciles a NamespaceClass object
 type NamespaceClassReconciler struct {
-    client.Client
-    Scheme *runtime.Scheme
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+func (r *NamespaceClassReconciler) getNamespacedResources() []string {
+	return []string{
+		ResourceServiceAccount,
+		ResourceNetworkPolicy,
+	}
+}
+
+func (r *NamespaceClassReconciler) genResource(s string, ns v1.Namespace) client.Object {
+	objectMeta := metav1.ObjectMeta{
+		Name:      ns.Name,
+		Namespace: ns.Name,
+	}
+
+	switch s {
+	case ResourceNetworkPolicy:
+		return &networkingv1.NetworkPolicy{
+			ObjectMeta: objectMeta,
+		}
+	case ResourceServiceAccount:
+		return &v1.ServiceAccount{
+			ObjectMeta: objectMeta,
+		}
+	default:
+		return nil
+	}
+}
+
+func (r *NamespaceClassReconciler) appendResourceConfig(s string, object client.Object, nsClass corev1.NamespaceClass) client.Object {
+	switch s {
+	case ResourceNetworkPolicy:
+		networkPolicy := object.(*networkingv1.NetworkPolicy)
+		networkPolicy.Spec = nsClass.Spec.NetworkPolicy
+		return networkPolicy
+	case ResourceServiceAccount:
+		sa := object.(*v1.ServiceAccount)
+		// config sa here ...
+		return sa
+	default:
+		return nil
+	}
 }
 
 type NSClassMap map[string]corev1.NamespaceClass
@@ -63,161 +108,243 @@ type NSClassMap map[string]corev1.NamespaceClass
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.20.2/pkg/reconcile
-func (r *NamespaceClassReconciler) Reconcile(ctx context.Context, req ctrl.Request) (res ctrl.Result, err error) {
-    _ = log.FromContext(ctx)
+func (r *NamespaceClassReconciler) Reconcile(ctx context.Context, _ ctrl.Request) (res ctrl.Result, err error) {
+	_ = log.FromContext(ctx)
 
-    // get all cluster Namespaces
-    var nsList v1.NamespaceList
-    if err = r.List(ctx, &nsList, &client.ListOptions{
-        LabelSelector: labels.Everything(),
-    }); err != nil {
-        if errors.IsNotFound(err) {
-            return res, nil
-        }
-        return res, err
-    }
+	nsList, err := r.getAllNS(ctx)
+	if err != nil {
+		log.Log.Error(err, "get all namespaces error")
+		return res, err
+	}
 
-    nsClassMap, err := r.getNSClassMap(ctx)
-    if err != nil {
-        log.Log.Error(err, "get namespace class map error")
-    }
-    for _, ns := range nsList.Items {
-        r.tackleOneNamespace(ctx, ns, nsClassMap)
-    }
+	nsClassMap, err := r.getNSClassMap(ctx)
+	if err != nil {
+		log.Log.Error(err, "get namespace class map error")
+	}
+	for _, ns := range nsList.Items {
+		r.tackleOneNamespace(ctx, ns, nsClassMap)
+	}
 
-    return res, nil
+	return res, nil
+}
+
+func (r *NamespaceClassReconciler) getAllNS(ctx context.Context) (nsList v1.NamespaceList, err error) {
+	if err = r.List(ctx, &nsList, &client.ListOptions{
+		LabelSelector: labels.Everything(),
+	}); err != nil {
+		if errors.IsNotFound(err) {
+			return nsList, nil
+		}
+		return nsList, err
+	}
+
+	return
 }
 
 func (r *NamespaceClassReconciler) getNSClassMap(ctx context.Context) (NSClassMap, error) {
-    var err error
-    nsClassMap := make(NSClassMap)
-    var nsClassList corev1.NamespaceClassList
-    if err = r.List(ctx, &nsClassList, &client.ListOptions{
-        LabelSelector: labels.Everything(),
-    }); err != nil {
-        if errors.IsNotFound(err) {
-            return nsClassMap, nil
-        }
-        return nsClassMap, err
-    }
-    for _, nsClass := range nsClassList.Items {
-        nsClassMap[nsClass.Name] = nsClass
-    }
+	var err error
+	nsClassMap := make(NSClassMap)
+	var nsClassList corev1.NamespaceClassList
+	if err = r.List(ctx, &nsClassList, &client.ListOptions{
+		LabelSelector: labels.Everything(),
+	}); err != nil {
+		if errors.IsNotFound(err) {
+			return nsClassMap, nil
+		}
+		return nsClassMap, err
+	}
+	for _, nsClass := range nsClassList.Items {
+		nsClassMap[nsClass.Name] = nsClass
+	}
 
-    return nsClassMap, nil
+	return nsClassMap, nil
 }
 
 func (r *NamespaceClassReconciler) tackleOneNamespace(ctx context.Context, ns v1.Namespace, nsClassMap NSClassMap) {
-    var err error
-    labelName := ns.Labels[NamespaceLabelName]
-    nsClass, ok := nsClassMap[labelName]
-    if !ok { // clean all useless network policy in this namespace
-        if err = r.delNetworkPolicy(ctx, ns); err != nil {
-            log.Log.Error(err, "")
-        }
-        return
-    }
+	// if the namespace has no label or invalid label value,
+	// it means that the namespace is not managed by this controller,
+	// should delete all related resources
+	nsClass, ok := nsClassMap[ns.Labels[NamespaceLabelName]]
+	if !ok {
+		if err := r.delNamespacedResource(ctx, ns); err != nil {
+			log.Log.Error(err, "")
+		}
+		return
+	}
 
-    if err = r.updateNetworkPolicy(ctx, ns, nsClass); err != nil {
-        log.Log.Error(err, "")
-    }
-    log.Log.Info(fmt.Sprintf("create/update network policy in namespace '%s' success", ns.Name))
+	// update or create namespaced resources
+	if err := r.updateNamespacedResource(ctx, ns, nsClass); err != nil {
+		log.Log.Error(err, "")
+		return
+	}
 }
 
-func (r *NamespaceClassReconciler) genNetworkPolicyIns(ns v1.Namespace) networkingv1.NetworkPolicy {
-    return networkingv1.NetworkPolicy{
-        ObjectMeta: metav1.ObjectMeta{
-            Name:      fmt.Sprintf("%s-networkpolicy", ns.Name),
-            Namespace: ns.Name,
-        },
-    }
+func (r *NamespaceClassReconciler) delNamespacedResource(ctx context.Context, ns v1.Namespace) (err error) {
+	key := client.ObjectKey{Namespace: ns.Name, Name: ns.Name}
+
+	for _, s := range r.getNamespacedResources() {
+		resource := r.genResource(s, ns)
+		if err = r.Get(ctx, key, resource); err != nil {
+			if !errors.IsNotFound(err) {
+				return fmt.Errorf("get %s in namespace '%s' error: %s", s, ns.Name, err)
+			}
+			return nil
+		}
+
+		if err = r.Delete(ctx, resource); err != nil {
+			err = fmt.Errorf("delete %s in namespace '%s' error: %s", s, ns.Name, err)
+		} else {
+			log.Log.Info(fmt.Sprintf("delete %s in namespace '%s' success", s, ns.Name))
+		}
+	}
+	return
 }
 
-func (r *NamespaceClassReconciler) delNetworkPolicy(ctx context.Context, ns v1.Namespace) (err error) {
-    networkPolicy := r.genNetworkPolicyIns(ns)
-
-    if err = r.Get(ctx, client.ObjectKey{Namespace: ns.Name, Name: networkPolicy.Name}, &networkingv1.NetworkPolicy{}); err != nil {
-        if !errors.IsNotFound(err) {
-            return fmt.Errorf("get network policy '%s' in namespace '%s' error: %s", networkPolicy.Name, ns.Name, err)
-        }
-        return nil
-    }
-
-    if err = r.Delete(ctx, &networkPolicy); err != nil {
-        err = fmt.Errorf("delete network policy '%s' in namespace '%s' error: %s", networkPolicy.Name, ns.Name, err)
-    }
-    log.Log.Info(fmt.Sprintf("delete network policy '%s' in namespace '%s' success", networkPolicy.Name, ns.Name))
-    return
+func (r *NamespaceClassReconciler) updateNamespacedResource(ctx context.Context, ns v1.Namespace, nsClass corev1.NamespaceClass) (err error) {
+	// create or update namespaced resources
+	key := client.ObjectKey{Namespace: ns.Name, Name: ns.Name}
+	for _, s := range r.getNamespacedResources() {
+		resource := r.genResource(s, ns)
+		resource = r.appendResourceConfig(s, resource, nsClass)
+		if err = r.Get(ctx, key, resource); err != nil {
+			if !errors.IsNotFound(err) {
+				return fmt.Errorf("get %T in namespace '%s' error: %s", resource, ns.Name, err)
+			}
+			if err = r.Create(ctx, resource); err != nil {
+				return fmt.Errorf("create %T in namespace '%s' error: %s", resource, ns.Name, err)
+			} else {
+				log.Log.Info(fmt.Sprintf("create %s in namespace '%s' success", s, ns.Name))
+			}
+		} else {
+			if err = r.Update(ctx, resource); err != nil {
+				return fmt.Errorf("update %T in namespace '%s' error: %s", resource, ns.Name, err)
+			} else {
+				log.Log.Info(fmt.Sprintf("update %s in namespace '%s' success", s, ns.Name))
+			}
+		}
+	}
+	return nil
 }
 
-func (r *NamespaceClassReconciler) updateNetworkPolicy(ctx context.Context, ns v1.Namespace, nsClass corev1.NamespaceClass) (err error) {
-    networkPolicy := r.genNetworkPolicyIns(ns)
+func (r *NamespaceClassReconciler) genNamespacePredicates() predicate.Funcs {
+	return predicate.Funcs{
+		CreateFunc: func(e event.CreateEvent) bool {
+			ctx := context.Background()
+			log.FromContext(ctx).Info("Namespace created", "name", e.Object.GetName())
+			obj, ok := e.Object.(*v1.Namespace)
+			if !ok {
+				log.Log.Error(nil, "expected a namespace object", "object", e.Object)
+				return false
+			}
+			nsClassMap, err := r.getNSClassMap(ctx)
+			if err != nil {
+				log.Log.Error(err, "get namespace class map error")
+				return false
+			}
+			if _, ok = nsClassMap[obj.Labels[NamespaceLabelName]]; !ok {
+				log.Log.Info("no namespace label matched")
+				return false
+			}
 
-    // delete network policy
-    if !nsClass.DeletionTimestamp.IsZero() { // Delete
-        return r.delNetworkPolicy(ctx, ns)
-    }
-
-    // create or update network policy
-    networkPolicy.Spec = nsClass.Spec.NetworkPolicy
-    if err = r.Get(ctx, client.ObjectKey{Namespace: ns.Name, Name: networkPolicy.Name}, &networkingv1.NetworkPolicy{}); err != nil {
-        if !errors.IsNotFound(err) {
-            return fmt.Errorf("get network policy '%s' in namespace '%s' error: %s", networkPolicy.Name, ns.Name, err)
-        }
-        if err = r.Create(ctx, &networkPolicy); err != nil {
-            return fmt.Errorf("create network policy '%s' in namespace '%s' error: %s", networkPolicy.Name, ns.Name, err)
-        }
-    } else {
-        if err = r.Update(ctx, &networkPolicy); err != nil {
-            return fmt.Errorf("update network policy '%s' in namespace '%s' error: %s", networkPolicy.Name, ns.Name, err)
-        }
-    }
-    return nil
+			return true
+		},
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			log.FromContext(context.TODO()).Info("Namespace updated", "name", e.ObjectNew.GetName())
+			oldObj, ok := e.ObjectOld.(*v1.Namespace)
+			if !ok {
+				log.Log.Error(nil, "expected a namespace object", "object", e.ObjectOld)
+				return false
+			}
+			newObj, ok := e.ObjectNew.(*v1.Namespace)
+			if !ok {
+				log.Log.Error(nil, "expected a namespace object", "object", e.ObjectNew)
+				return false
+			}
+			if oldObj.Labels[NamespaceLabelName] == newObj.Labels[NamespaceLabelName] {
+				log.Log.Info("no label changes")
+				return false
+			}
+			return true
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			log.FromContext(context.TODO()).Info("Namespace deleted", "name", e.Object.GetName())
+			return false // do nothing
+		},
+	}
 }
 
-func genNamespacePredicates() predicate.Funcs {
-    return predicate.Funcs{
-        CreateFunc: func(e event.CreateEvent) bool {
-            log.FromContext(context.TODO()).Info("Namespace created", "name", e.Object.GetName())
-            return true
-        },
-        UpdateFunc: func(e event.UpdateEvent) bool {
-            log.FromContext(context.TODO()).Info("Namespace updated", "name", e.ObjectNew.GetName())
-            return true
-        },
-        DeleteFunc: func(e event.DeleteEvent) bool {
-            log.FromContext(context.TODO()).Info("Namespace deleted", "name", e.Object.GetName())
-            return true
-        },
-    }
+func (r *NamespaceClassReconciler) genNamespaceClassPredicates() predicate.Funcs {
+	return predicate.Funcs{
+		CreateFunc: func(e event.CreateEvent) bool {
+			log.FromContext(context.TODO()).Info("Namespace Class created", "name", e.Object.GetName())
+			return true
+		},
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			log.FromContext(context.TODO()).Info("Namespace Class updated", "name", e.ObjectNew.GetName())
+			// TODO: I think should check if changed the name
+			return true
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			log.FromContext(context.TODO()).Info("Namespace Class deleted", "name", e.Object.GetName())
+			nsClass, ok := e.Object.(*corev1.NamespaceClass)
+			if !ok {
+				log.Log.Error(nil, "expected a namespace class object", "object", e.Object)
+				return false
+			}
+
+			ctx := context.Background()
+			nsList, err := r.getAllNS(ctx)
+			if err != nil {
+				log.Log.Error(err, "get all namespaces error")
+				return false
+			}
+			for _, ns := range nsList.Items {
+				if ns.Labels[NamespaceLabelName] != nsClass.Name {
+					continue
+				}
+				if err = r.delNamespacedResource(ctx, ns); err != nil {
+					log.Log.Error(err, "delete namespaced resource error")
+				}
+			}
+
+			return false
+		},
+	}
+}
+
+func (r *NamespaceClassReconciler) watchNamespaceFun() handler.MapFunc {
+	return func(ctx context.Context, obj client.Object) []reconcile.Request {
+		// get all NamespacesClass
+		nsClassMap, err := r.getNSClassMap(ctx)
+		if err != nil {
+			log.Log.Error(err, "get namespace class map error")
+			return []reconcile.Request{}
+		}
+
+		// get modified Namespace
+		ns, ok := obj.(*v1.Namespace)
+		if !ok {
+			log.Log.Error(nil, "expected a namespace object", "object", obj)
+			return []reconcile.Request{}
+		}
+
+		r.tackleOneNamespace(ctx, *ns, nsClassMap)
+		return []reconcile.Request{}
+	}
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *NamespaceClassReconciler) SetupWithManager(mgr ctrl.Manager) error {
-    return ctrl.NewControllerManagedBy(mgr).
-        Watches(
-            &v1.Namespace{},
-            handler.EnqueueRequestsFromMapFunc(func(ctx context.Context, a client.Object) []reconcile.Request {
-                // get all NamespacesClass
-                nsClassMap, err := r.getNSClassMap(ctx)
-                if err != nil {
-                    log.Log.Error(err, "get namespace class map error")
-                    return []reconcile.Request{}
-                }
-
-                // get modified Namespace
-                ns, ok := a.(*v1.Namespace)
-                if !ok {
-                    log.Log.Error(nil, "expected a namespace object", "object", a)
-                    return []reconcile.Request{}
-                }
-
-                //
-                r.tackleOneNamespace(ctx, *ns, nsClassMap)
-                return []reconcile.Request{}
-            }),
-            builder.WithPredicates(genNamespacePredicates())).
-        For(&corev1.NamespaceClass{}).
-        Named("namespaceclass").
-        Complete(r)
+	return ctrl.NewControllerManagedBy(mgr).
+		Watches(
+			&v1.Namespace{},
+			handler.EnqueueRequestsFromMapFunc(r.watchNamespaceFun()),
+			builder.WithPredicates(r.genNamespacePredicates()),
+		).
+		For(
+			&corev1.NamespaceClass{},
+			builder.WithPredicates(r.genNamespaceClassPredicates()),
+		).
+		Named("namespaceclass").
+		Complete(r)
 }
